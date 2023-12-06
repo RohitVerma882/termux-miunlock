119abefe1271079f062486d0fad5643e05dc85a0
Bruno Gonçalves <bigbruno@gmail.com>
2023-12-06 06:10:43 -0300

    Added interface in zenity and dialog, automations were also added to detect the product and the token, in addition to executing commands to perform the unlock. *** The part of the commands to unlock has not yet been tested, as I need to wait a few days until Xiaomi releases the smartphone I am using to be unlocked

--- setup.sh
+++ setup.sh
@@ -1,17 +1,29 @@
-#!/data/data/com.termux/files/usr/bin/bash
+#!/usr/bin/env bash
 
-apt-get update
-apt-get --assume-yes upgrade
-apt-get --assume-yes install coreutils gnupg wget termux-api openjdk-17 vim
-if [ ! -f "$PREFIX/etc/apt/sources.list.d/mi-fastboot.list" ]; then
-  mkdir -p $PREFIX/etc/apt/sources.list.d
-  echo -e "deb https://rohitverma882.github.io termux extras" > $PREFIX/etc/apt/sources.list.d/mi-fastboot.list
-  wget -qP $PREFIX/etc/apt/trusted.gpg.d https://rohitverma882.github.io/rohit.gpg
-  apt update
-  apt install mi-fastboot
+if type termux-info &> /dev/null; then
+
+  apt-get update
+  apt-get --assume-yes upgrade
+  apt-get --assume-yes install coreutils gnupg wget termux-api openjdk-17 vim
+  if [ ! -f "$PREFIX/etc/apt/sources.list.d/mi-fastboot.list" ]; then
+    mkdir -p $PREFIX/etc/apt/sources.list.d
+    echo -e "deb https://rohitverma882.github.io termux extras" > $PREFIX/etc/apt/sources.list.d/mi-fastboot.list
+    wget -qP $PREFIX/etc/apt/trusted.gpg.d https://rohitverma882.github.io/rohit.gpg
+    apt update
+    apt install mi-fastboot
+  else
+    echo "Repo already installed"
+    apt install mi-fastboot
+  fi
+
+  echo "done!"
+
+elif type fastboot &> /dev/null; then
+
+  echo "Fastboot is already installed"
+
 else
-  echo "Repo already installed"
-  apt install mi-fastboot
-fi
 
-echo "done!"
+  echo "I didn't find fastboot, please install it before proceeding."
+  
+fi
